f56ef9728e7e739c5b2b7a8b83e93ddb58e65dd8
Daniel <turutadaniel20@gmail.com>
2024-05-17 22:21:13 +0300

    fixed sign in

--- backend/src/main/java/com/example/backend/Controllers/SecurityController.java
+++ backend/src/main/java/com/example/backend/Controllers/SecurityController.java
@@ -6,6 +6,7 @@ import com.example.backend.Services.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,8 @@ public class SecurityController {
                 throw new RuntimeException(e);
             }
         }
-        else return ResponseEntity.status(401).body("Bad credentials");
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Bad credentials");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 }
