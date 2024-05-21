b57dd4e99f31f184721a29a3f8ca604c74237893
Daniel <turutadaniel20@gmail.com>
2024-05-22 00:41:18 +0300

    pulling cars to front end

--- frontend/src/User/User.tsx
+++ frontend/src/User/User.tsx
@@ -1,5 +1,21 @@
+import {useEffect, useState} from "react";
+import {isAxiosError} from "axios";
+import axiosInstance from "../Axios/axiosInstance";
 
 const User = () => {
+
+    const [cars, setCars] = useState([]);
+
+    const fetchCars = async () => {
+        const response = await axiosInstance.get('http://localhost:8081/cars/all');
+        setCars(response.data);
+    }
+
+    useEffect(() => {
+        fetchCars();
+        console.log(cars);
+    }, []);
+
     return (
         <>
         <p>
